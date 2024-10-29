2a7ba47a0ca9ce3ba662e91a0a11b0d411307e76
LNeuman91 <lneuman819@gmail.com>
2024-10-28 19:50:04 -0700

    Initial commit with Color Picker project

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+
+const App = () => {
+  const [selectedColor, setSelectedColor] = useState("");
+
+  return (
+    <div>
+      <div id="navbar">
+        <div>Currently selected: </div>
+        <div className={selectedColor}>{selectedColor}</div>
+      </div>
+      
+      <div id="colors-list">
+        <Color color="yellow" setSelectedColor={setSelectedColor} />
+        <Color color="green" setSelectedColor={setSelectedColor} />
+        <Color color="black" setSelectedColor={setSelectedColor} />
+      </div>
+    </div>
+  );
+};
+
+const Color = ({ color, setSelectedColor }) => {
+  return <div className={color} onClick={() => setSelectedColor(color)}></div>;
+};
+
+export default App;
