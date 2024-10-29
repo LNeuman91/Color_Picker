1b292140aa568ac1fe768fda13b396f1cfe073cc
LNeuman91 <lneuman819@gmail.com>
2024-10-29 09:19:21 -0700

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const App = () => {
   const [selectedColor, setSelectedColor] = useState("");
@@ -9,12 +9,13 @@ const App = () => {
         <div>Currently selected: </div>
         <div className={selectedColor}>{selectedColor}</div>
       </div>
-      
+
       <div id="colors-list">
         <Color color="yellow" setSelectedColor={setSelectedColor} />
         <Color color="green" setSelectedColor={setSelectedColor} />
         <Color color="black" setSelectedColor={setSelectedColor} />
       </div>
+      <p>hello</p>
     </div>
   );
 };
